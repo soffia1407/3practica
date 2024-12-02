af8bf29ee246dff0d676e1db46a7a7aecb3cc64a
soffia1407 <sff140705@gmail.com>
2024-12-03 00:51:45 +0700

    Using Mixins + Refactoring of views

--- snippets/views.py
+++ snippets/views.py
@@ -1,49 +1,41 @@
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
+from rest_framework.decorators import api_view
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework import mixins
+from rest_framework import generics
 from .models import Snippet
 from .serializers import SnippetSerializer
-@csrf_exempt
-def snippet_list(request):
-   """
-   List all code snippets, or create a new snippet.
-   """
-   if request.method == 'GET':
-       snippets = Snippet.objects.all()
-       serializer = SnippetSerializer(snippets, many=True)
-       return JsonResponse(serializer.data, safe=False)
 
-   elif request.method == 'POST':
-       data = JSONParser().parse(request)
-       serializer = SnippetSerializer(data=data)
-       if serializer.is_valid():
-           serializer.save()
-           return JsonResponse(serializer.data, status=201)
-       return JsonResponse(serializer.errors, status=400)
+class SnippetList(mixins.ListModelMixin,
+                 mixins.CreateModelMixin,
+                 generics.GenericAPIView):
+   queryset = Snippet.objects.all()
+   serializer_class = SnippetSerializer
+
+   def get(self, request, *args, **kwargs):
+       return self.list(request, *args, **kwargs)
+
+   def post(self, request, *args, **kwargs):
+       return self.create(request, *args, **kwargs)
 
 
-@csrf_exempt
-def snippet_detail(request, pk):
-   """
-   Retrieve, update or delete a code snippet.
-   """
-   try:
-       snippet = Snippet.objects.get(pk=pk)
-   except Snippet.DoesNotExist:
-       return HttpResponse(status=404)
+class SnippetDetail(mixins.RetrieveModelMixin,
+                   mixins.UpdateModelMixin,
+                   mixins.DestroyModelMixin,
+                   generics.GenericAPIView):
+   queryset = Snippet.objects.all()
+   serializer_class = SnippetSerializer
 
-   if request.method == 'GET':
-       serializer = SnippetSerializer(snippet)
-       return JsonResponse(serializer.data)
+   def get(self, request, *args, **kwargs):
+       return self.retrieve(request, *args, **kwargs)
 
-   elif request.method == 'PUT':
-       data = JSONParser().parse(request)
-       serializer = SnippetSerializer(snippet, data=data)
-       if serializer.is_valid():
-           serializer.save()
-           return JsonResponse(serializer.data)
-       return JsonResponse(serializer.errors, status=400)
+   def put(self, request, *args, **kwargs):
+       return self.update(request, *args, **kwargs)
 
-   elif request.method == 'DELETE':
-       snippet.delete()
-       return HttpResponse(status=204)
+   def delete(self, request, *args, **kwargs):
+       return self.destroy(request, *args, **kwargs)
